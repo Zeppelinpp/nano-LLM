0f0804d652587b2e145d13e0e3593bd4c7ba7438
Zeppelinpp <1056118649@qq.com>
2026-02-07 13:05:29 +0800

    bert model test

--- model/bert.py
+++ model/bert.py
@@ -1,13 +1,16 @@
-from transformers import BertModel
-from transformers import BertTokenizer
+from transformers import BertModel, BertTokenizer
 
 
-bert_model = "bert-base-uncased"
+# 使用中文 BERT 模型
+bert_model = "bert-base-chinese"
 model = BertModel.from_pretrained(bert_model, cache_dir="../public")
-
 tokenizer = BertTokenizer.from_pretrained(bert_model, cache_dir="../public")
 
-sentence = "床前明"
+sentence = "床前明月光"
+tokens = tokenizer.tokenize(sentence)
+print(tokens)
 
-input_ids = tokenizer.tokenize(sentence)
-print(input_ids)
+# 如果需要获取 token IDs
+input_ids = tokenizer.encode(sentence, add_special_tokens=True)
+print("Input IDs:", input_ids)
+print("Decoded:", tokenizer.decode(input_ids))
