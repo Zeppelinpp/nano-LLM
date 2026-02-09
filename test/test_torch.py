a5e9353f1d2eff310b5927ee2b4fac3fe06a0266
Zeppelinpp <1056118649@qq.com>
2026-02-09 13:54:06 +0800

    fix: upper triangular mask
    [[0,1,1],[0,0,1],[0,0,0]]

--- test/test_torch.py
+++ test/test_torch.py
@@ -31,7 +31,7 @@ print(f"After split attention heads, each heads: {q.shape}")
 # batch_size, num_heads, seq_len, head_dim
 
 att = q @ k.transpose(2, 3) * (1.0 / math.sqrt(k.size(-1)))
-casual_mask = torch.tril(torch.ones(10, 10)).view(1, 1, 10, 10).bool().to(device)
+casual_mask = (torch.tril(torch.ones(10, 10)).view(1, 1, 10, 10) == 0).to(device)
 att = att.masked_fill(casual_mask, float("-inf"))
 att = F.softmax(att, dim=-1)
 y = att @ v
